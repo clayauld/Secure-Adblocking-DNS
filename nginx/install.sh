fcba91e1c1ac0d2179d65dc4c1f359ec163b179e
Clayton Auld <clayauld@gmail.com>
2021-10-05 16:24:51 -0800

    Updated install script for nginx settings

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 echo
-echo "This script installs the transparent-proxy.service to systemd and transparent-proxy script to /usr/local/bin"
+echo "This script installs the required components to set up an Nginx tranparent proxy for DNS-over-TLS and Pi-Hole"
+echo "PLEASE BE AWARE THIS WILL OVERWRITE YOUR CURRENT NGINX CONFIG!!"
+echo "However, it will back up your old config in case you need to revert."
 echo
+curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/nginx.conf > /etc/nginx/ngxin.conf
+mkdir /etc/nginx/streams
+curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/streams/DoT_dns > /etc/nginx/streams/DoT_dns
+systemctl restart nginx
 curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/transparent-proxy.service > /lib/systemd/system/transparent-proxy.service
 curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/transparent-proxy.sh > /usr/local/bin/transparent-proxy
 sudo systemctl daemon-reload
