c7b08cff2d740e77923de466fded5dab864ab790
Clayton Auld <clayauld@gmail.com>
2021-10-05 18:21:16 -0800

    updated nginx/install.sh to back up nginx.conf

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 echo
 echo "This script installs the required components to set up an Nginx tranparent proxy for DNS-over-TLS and Pi-Hole"
+echo
 echo "PLEASE BE AWARE THIS WILL OVERWRITE YOUR CURRENT NGINX CONFIG!!"
+echo
 echo "However, it will back up your old config in case you need to revert."
 echo
-curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/nginx.conf > /etc/nginx/ngxin.conf
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/nginx.conf > /etc/nginx/nginx.conf
 mkdir /etc/nginx/streams
 curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/streams/DoT_dns > /etc/nginx/streams/DoT_dns
 systemctl restart nginx
