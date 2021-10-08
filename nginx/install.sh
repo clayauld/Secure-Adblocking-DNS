518112743d22ab9be26a25ff3e781039b0f785be
Clayton Auld <clayauld@gmail.com>
2021-10-08 09:53:54 -0800

    Updated install script for nginx settings to fix issues in issue #4

--- nginx/install.sh
+++ nginx/install.sh
@@ -6,12 +6,12 @@ echo "PLEASE BE AWARE THIS WILL OVERWRITE YOUR CURRENT NGINX CONFIG!!"
 echo
 echo "However, it will back up your old config in case you need to revert."
 echo
-sudo apt install nginx
+sudo apt install -y nginx
 cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/nginx.conf > /etc/nginx/nginx.conf
 mkdir /etc/nginx/streams
 curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/streams/DoT_dns > /etc/nginx/streams/DoT_dns
-systemctl restart nginx
+#systemctl restart nginx
 curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/transparent-proxy.service > /lib/systemd/system/transparent-proxy.service
 curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/transparent-proxy.sh > /usr/local/bin/transparent-proxy
 sudo systemctl daemon-reload
