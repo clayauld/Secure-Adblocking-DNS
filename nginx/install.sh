e46bc73d14f74e17fa368d7725fc014e2c68dc14
Clayton Auld <clayauld@gmail.com>
2021-10-13 17:54:00 -0800

    Update nginx/install.sh with bug fix for bug #4.

--- nginx/install.sh
+++ nginx/install.sh
@@ -14,6 +14,7 @@ curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/
 #systemctl restart nginx
 curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/transparent-proxy.service > /lib/systemd/system/transparent-proxy.service
 curl -s https://raw.githubusercontent.com/clayauld/Secure-Adblocking-DNS/master/nginx/transparent-proxy.sh > /usr/local/bin/transparent-proxy
+sudo chmod +x /usr/local/bin/tranparent-proxy
 sudo systemctl daemon-reload
 sudo systemctl enable transparent-proxy.service
 echo Finished!
